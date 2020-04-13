b3c7699a8ce801e5d52bf6ac9251b4507cf317df
Michał Pawełczyk <michal@localhost.localdomain>
2020-04-13 13:52:35 +0200

    reimport project to intelliJ helps a lot. starts working

--- src/main/java/guru/springframework/joke/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -1,14 +1,12 @@
 package guru.springframework.joke;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import static org.springframework.boot.SpringApplication.run;
-
 
 @SpringBootApplication
 public class JokeappApplication {
 
 	public static void main(String[] args) {
-		run(JokeappApplication.class, args);
+		SpringApplication.run(JokeappApplication.class, args);
 	}
 }
